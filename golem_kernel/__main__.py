c83f9ff8d0e4a1d0e30cd45f1d34543af7c2586e
Jan Betley <jan.betley@golem.network>
2023-02-22 19:05:46 +0100

    python3 -m golem_kernel install [--user]

--- golem_kernel/__main__.py
+++ golem_kernel/__main__.py
@@ -1,5 +1,50 @@
+import json
+import os
+import sys
+import tempfile
+
 from ipykernel.kernelapp import IPKernelApp
+from jupyter_client.kernelspec import KernelSpecManager
 
 from .golem_kernel import GolemKernel
 
-IPKernelApp.launch_instance(kernel_class=GolemKernel)
+KERNEL_DICT = {
+    "argv": ["python", "-m", "golem_kernel", "-f", "{connection_file}"],
+    "display_name": "Golem",
+    "language": "python",
+}
+
+def parse_install_argv():
+    if len(sys.argv) == 2:
+        return False
+    elif len(sys.argv) == 3 and sys.argv[2] == "--user":
+        return True
+    else:
+        raise ValueError(f"Incorrect args: {sys.argv[2:]}. Only accepted option is --user.")
+
+def install():
+    args = {"kernel_name": "golem"}
+
+    user = parse_install_argv()
+    if user:
+        args["user"] = True
+    else:
+        args["prefix"] = sys.prefix
+
+    with tempfile.TemporaryDirectory() as path:
+        with open(os.path.join(path, "kernel.json"), "w") as f:
+            json.dump(KERNEL_DICT, f, indent=1)
+
+        args["source_dir"] = path
+
+        kernel_spec_manager = KernelSpecManager()
+        dest = kernel_spec_manager.install_kernel_spec(**args)
+
+    print(f"Installed kernelspec Golem in {dest}")
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == "install":
+        install()
+    else:
+        IPKernelApp.launch_instance(kernel_class=GolemKernel)
